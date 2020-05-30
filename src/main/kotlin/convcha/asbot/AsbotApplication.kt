6aa1af7c0a2ba437ffe5a5e440d67cc22217a851
convcha <convcha@gmail.com>
2020-05-30 23:22:02 +0900

    Initial commit

---
+++ src/main/kotlin/convcha/asbot/AsbotApplication.kt
@@ -0,0 +1,11 @@
+package convcha.asbot
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class AsbotApplication
+
+fun main(args: Array<String>) {
+	runApplication<AsbotApplication>(*args)
+}
---
+++ src/test/kotlin/convcha/asbot/AsbotApplicationTests.kt
@@ -0,0 +1,13 @@
+package convcha.asbot
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class AsbotApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
